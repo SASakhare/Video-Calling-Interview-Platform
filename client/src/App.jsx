df09948a3a95d08951195fe6bc4911fa1ca6a698
sejal <sakharwsejal@gmail.com>
2026-06-27 10:26:28 +0530

    auth setup and some backend auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import { Show, SignIn, SignInButton, SignUpButton, UserButton } from '@clerk/react';
 import React from 'react'
 
 export const App = () => {
   return (
-    <div>APP</div>
+    <div>
+      <h1>Welcome to My App</h1>
+      <Show when="signed-out">
+        <SignInButton mode='modal' />
+        <SignUpButton mode='modal' />
+      </Show>
+      <Show when="signed-in">
+        <UserButton/>
+      </Show>
+    </div>
   )
 }
 
